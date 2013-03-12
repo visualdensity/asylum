e44ea0141f7d01437225ff482fc00b3100c559f3
Wee Keat Chin <weekeat@visualdensity.com>
2013-03-12 21:41:55 +1100

    bubble sort v1

--- src/algorithms/sort_bubble.php
+++ src/algorithms/sort_bubble.php
@@ -1,2 +1,40 @@
 <?php
-// Bubble sort
+/**
+ * Classic bubble sort
+ *
+ * Pseudo code:
+ *
+ *   func bubblesort( var a as array )
+ *     for i from 1 to N
+ *         for j from 0 to N - 1
+ *            if a[j] > a[j + 1]
+ *               swap( a[j], a[j + 1] )
+ *   end func
+ *
+ * Reference:
+ * http://www.algorithmist.com/index.php/Bubble_sort
+ */
+
+function bubbleSort($array) {
+
+    if( !is_array($array) ) {
+        return false;
+    }
+
+    $count = sizeof($array);
+    for( $c=0; $c < $count-1; $c++ ) {
+        for( $i=0; $i < $count-1; $i++) {
+            if($array[$i] > $array[$i+1]) {
+                $temp = $array[$i+1];
+
+                $array[$i+1] = $array[$i];
+                $array[$i]   = $temp;
+            }
+        }
+    }
+
+    return $array;
+}
+
+$input = str_split('3748659012');
+print_r(bubbleSort($input));
