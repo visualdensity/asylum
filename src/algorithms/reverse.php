e66ef99c666115678c7e944a3a86de7213885c83
Wee Keat Chin <weekeat@visualdensity.com>
2013-03-12 15:31:04 +1100

    Added algo folder with reverse string inplace swap

---
+++ src/algorithms/reverse.php
@@ -0,0 +1,29 @@
+<?php
+// Reverse a string
+
+function reverse($chars) {
+
+    $charsSize   = count($chars);
+    $charsToSwap = floor($charsSize/2);
+    $decrementor = $charsSize-1;
+
+    for($incrementor=0; $incrementor < $charsToSwap; $incrementor++) {
+
+        $front = $chars[$incrementor];
+
+        //in place swap
+        $chars[$incrementor] = $chars[$decrementor];
+        $chars[$decrementor] = $front;
+
+        $decrementor--;
+    }
+
+    return $chars;
+}
+
+//example
+$string = 'twinkle twinkle little star, how I wonder what you are, up above the world so high, like a diamond in the sky';
+$reversed = reverse(str_split($string));
+
+print "Input: ";  print $string;            print PHP_EOL;
+print "Output: "; print implode($reversed); print PHP_EOL;
