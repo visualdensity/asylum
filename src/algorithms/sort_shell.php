6c0c36e1a49c978fa685810150eb519a46e6f402
Wee Keat Chin <weekeat@visualdensity.com>
2013-03-19 18:56:36 +1100

    Shell sort - ripped from somewhere

--- src/algorithms/sort_shell.php
+++ src/algorithms/sort_shell.php
@@ -1,2 +1,29 @@
 <?php
-// Shell sort
+/**
+ * Shell sort
+ *
+ * Ripped from somewhere - forgotten where exactly. Needs to re-examine this 
+ * sometime in the near future
+ */
+
+function shellSort($arr)
+{
+    $inc = round(count($arr)/2);
+    while($inc > 0)
+    {
+        for($i = $inc; $i < count($arr);$i++){
+            $temp = $arr[$i];
+            $j = $i;
+            while($j >= $inc && $arr[$j-$inc] > $temp)
+            {
+                $arr[$j] = $arr[$j - $inc];
+                $j -= $inc;
+            }
+            $arr[$j] = $temp;
+        }
+        $inc = round($inc/2.2);
+    }
+    return $arr;
+}
+
+print_r( shellSort(str_split('8847948282771')) );
